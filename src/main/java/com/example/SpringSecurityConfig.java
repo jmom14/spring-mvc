7067238b896da51af3d38afbb3e1a133043e1e16
jmom14 <manuel.marquina.14@gmail.com>
2019-03-22 19:53:36 -0600

    class 112

--- src/main/java/com/example/SpringSecurityConfig.java
+++ src/main/java/com/example/SpringSecurityConfig.java
@@ -2,6 +2,8 @@ package com.example;
 
 
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -21,6 +24,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	private LoginSuccessHandler successHandler;
+	
+	@Autowired
+	private DataSource datasource;
+	
+	@Autowired
+	private BCryptPasswordEncoder  passwordEncoder;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -45,12 +54,24 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder build) throws Exception {
 
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        UserBuilder users = User.builder().passwordEncoder(encoder::encode);
 		
-        build.inMemoryAuthentication()
-        .withUser(users.username("admin").password("12345").roles("ADMIN" , "USER"))
-        .withUser(users.username("jose").password("12345").roles("USER"));
+			
+		build.jdbcAuthentication()
+		.dataSource(datasource)
+		.passwordEncoder(passwordEncoder)
+		.usersByUsernameQuery("select username, password, enabled from users where username =?")
+		.authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on (a.user_id = u.id) where username = ?");
+		
+		
+		/*
+		 * PasswordEncoder encoder =
+		 * PasswordEncoderFactories.createDelegatingPasswordEncoder(); UserBuilder users
+		 * = User.builder().passwordEncoder(encoder::encode);
+		 * 
+		 * build.inMemoryAuthentication()
+		 * .withUser(users.username("admin").password("12345").roles("ADMIN" , "USER"))
+		 * .withUser(users.username("jose").password("12345").roles("USER"));
+		 */
         
 	}
 
